fc680ce8590d61d6d2cf80f37bfcb4c7e7782919
Manisha S. Jadhav <52768200+msjadhav03@users.noreply.github.com>
2021-06-16 17:08:42 +0530

    Add files via upload

--- playground.js
+++ playground.js
@@ -1,143 +1,106 @@
-const formulas = require('./formulas')
-
-//console.log(JSON.stringify(formulas.formulas.length))
-
-const realFeature = {
-    features:{
-        current:{
-            data:
-            [
-                {
-                    value: 0.4
-                },
-                {
-                    value : 0.4
-                }
-            ]
-        },
-        voltage:
-        {
-            data:
-            [
-                {
-                    value : 0.5
-                },
-                {
-                    value : 0.5
-                }
-            ]
-        }
-    }
-}
-
-const values = Object.keys(realFeature.features)
-//console.log(values)
-let formula = ""
-for(let i=0;i<formulas.formulas.length;i++)
+const equation = "[(1+2)*3]";
+const result = eval(equation)
+const feature = 
 {
-   
-    const a = values.length
-    let count = 0
-    //console.log(a)
-    //console.log(formulas.formulas[i].independant)
-    if(a == formulas.formulas[i].independant.length)
-    {    
-        for(let j=0;j<values.length;j++)
-        {
-            for(let k = 0;k<values.length;k++)
-            {   
-                if(values[j]===formulas.formulas[i].independant[k])
-                {
-                    count ++
-                }
-            }
-              
-        
-        }
-           // console.log(count)
-            if(count === a)
+    current:
+    {
+        data:
+        [
             {
+                value: 0.4
+            },
+            {
+                value: 0.3
+            },
+            {
+                value:22
+            }
+        ]
 
-                formula = formulas.formulas[i].formula
-                
-                //console.log(typeof(formulas.formulas[i].formula))
-                break;
-            } 
-   
+    },
+    voltage:
+    {
+        data:
+        [
+            {
+                value:5
+            },
+            {
+                value: 5
+            },
+            {
+                value: 55
+            },
+            {
+                value: 7
+            },
+            {
+                value: 8
+            }
+        ]
     }
 }
 
+// splitting formula into array of variable
+const power = "{[(current*voltage)]}"
+var expr = new RegExp("(?<=[{(-+*/)}])|(?=[{(-+*/)}])")  
+console.log(expr)
+const powerArray = power.split(expr)
+console.log(powerArray)
 
-console.log(formula)
 
+// last data value in current and voltage
 
-/*
-    k=0
-    for(i=0;i<formula.length;i++)
+const lengthOfCurrentData = feature.current.data.length - 1
+const lengthOfVoltageData = feature.voltage.data.length - 1
+
+// comparing splitted array with real feature 
+console.log(Object.keys(feature))
+
+// first for loop unitl length of realFeature keys array length
+// second loop array until length of splitted array formula length
+for(let i =0 ;i< Object.keys(feature).length;i++)
+{
+    for(let j = 0;j<powerArray.length;j++)
     {
-
-        if(formula[i]==='*' || formula[i]==='/'||formula[i]==='+'||formula[i]==='-'||formula[i]==='('||formula[i]===')')
+        if(Object.keys(feature)[i] === powerArray[j])
         {
-            c++;
-            a[k] = formula[i]
+            console.log('Inside nested loop')
+            const variableDataLength = feature[Object.keys(feature)[i]].data.length - 1
+            console.log(feature[(Object.keys(feature)[i])].data[variableDataLength].value)
+           
+            //console.log(variableDataLength)
+            //console.log(feature[Object.keys(feature)[i]])
+            //console.log(feature[(Object.keys(feature)[i])].data[(feature[Object.keys(feature)[i]].length) - 1])
+            powerArray[j] = feature[(Object.keys(feature)[i])].data[variableDataLength].value
+
+            // TO DO add console.log to above statement to check whether it will work or not
         }
     }
-*/
-// current.data[current.data.length - 1] * volatage.data[voltage.data.length - 1]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// formula[i].independant === values
-//console.log(JSON.stringify(formulas.formulas[1].independant))
-
-/*for(let i = 0;i<formulas.formulas.length;i++)
-{
-    if(formulas.formulas[i].independant == values)
-    {
-        console.log(formulas.formulas[i].formula)
-    }else
-    {
-        console.log("formula not found")
-    }
-}*/
-
-/*for(let j = 0;j< formulas.formulas.length;j++)
-{
-    console.log((formulas.formulas[j].independant).length)
-    console.log(typeof(values.length))
-    console.log(typeof(formulas.formulas[j].independant.length))
-    let result = values.foreach((element)=>
-    {
-        console.log('entered in loop')
-        return JSON.stringify(formulas.formulas[j].independant).includes(element)
-    })
-    console.log(result)
-    if(result===true)
-    {
-        console.log(formulas.formulas[j].formula)
-        break;
-    }else
-    {
-        console.log('formula not found')
-    }
 }
-console.log(values)
-*/
+console.log("after loop")
+// convert to powerArray to string
+const powerArrayString = powerArray.toString()
+console.log("array to string",powerArrayString)
+//          replace commas from converted string
+//          for(let i = 0;i<powerArrayString.length;i++)
+//          {
+//              if(powerArrayString[i]==',')
+//              {
+//                  powerArrayString[i] = ' '
+//              }
+//          }
+const powerArrayStringwithoutComma = powerArrayString.replace(/,/g, '')
+// print powerArrayStringwithoutComma 
+console.log(powerArrayStringwithoutComma)
+// evaluate string
+const evalPowerArrayStringwithoutComma = eval(powerArrayStringwithoutComma)
+console.log(evalPowerArrayStringwithoutComma)
+//console.log("answer after applying toString() to powerArray and also eval",eval(powerArray.toString().replace(',','')))
 
 
-
+//  console.log(feature.current.data[lengthOfCurrentData].value)
+const equationWithVariable = console.log(eval("[(feature.current.data[lengthOfCurrentData].value*feature.voltage.data[lengthOfVoltageData].value)]"))
+//  const result2 = eval(equationWithVariable)
+//  console.log(result2)
+//  console.log(result)
